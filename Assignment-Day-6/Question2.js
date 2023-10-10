bc60737a483deb73d9ab4ee0328b5b11f8578991
rounak-successive <rounak.dwary@successive.tech>
2023-10-10 12:10:46 +0530

    Assignment 6

---
+++ Assignment-Day-6/Question2.js
@@ -0,0 +1,40 @@
+// Create a class Shape with properties width and height and methods getArea(). Create two classes Rectangle and Triangle that inherit from the Shape class and implement the getArea() method for their respective shapes.
+
+class Shape {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+  }
+
+  getArea() {
+    return NaN
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(width, height) {
+    super(width, height)
+  }
+
+  getArea() {
+    return this.width * this.height
+  }
+}
+
+class Triangle extends Shape {
+  constructor(width, height) {
+    super(width, height)
+  }
+
+  getArea() {
+    return (this.width * this.height) / 2
+  }
+}
+
+const rectangle = new Rectangle(4, 5),
+  triangle = new Triangle(5, 8),
+  shape = new Shape(2, 3)
+
+console.log('The area of the rectangle is: ', rectangle.getArea())
+console.log('The area of the triangle is:', triangle.getArea())
+console.log('The area of the shape is :', shape.getArea())
