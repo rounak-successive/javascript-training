f214c487551376f7a02ae627137ba6f52b7b995c
rounak-successive <rounak.dwary@successive.tech>
2023-10-25 12:13:38 +0530

    added Assignment-day-9

---
+++ Assignment-Day-9/Math/Math.js
@@ -0,0 +1,7 @@
+// 3.create a new folder named 'math' and create a file math.js. In math.js, define two functions:
+// a. add(a, b): takes two numbers as arguments and returns their sum.
+// b. subtract(a, b): takes two numbers as arguments and returns their difference.
+// create a new file index.js and just export those functions and then use those function in any of the file
+
+export const add = (a, b) => a + b
+export const subtract = (a, b) => a - b
---
+++ Assignment-Day-9/Question2.js
@@ -0,0 +1,11 @@
+// 1.create repo and add Readme.md file
+
+// 2.Install the lodash package using npm install lodash. and use any of its function
+
+import _ from 'lodash'
+
+const array = [1]
+const other = _.concat(array, 2, [3], [[4]])
+
+console.log(array)
+console.log(other)
